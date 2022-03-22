0fbb6327b48ab1423551f24393f0c8537e50443b
TxcA <447358255@qq.com>
2022-03-22 22:23:51 +0800

    added(CenterImageSpan): 新增可修改DrawableSize

--- spannable/src/main/java/com/drake/spannable/span/CenterImageSpan.kt
+++ spannable/src/main/java/com/drake/spannable/span/CenterImageSpan.kt
@@ -7,9 +7,33 @@ import android.graphics.Paint
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.text.style.ImageSpan
+import com.drake.spannable.model.drawableSize
+import java.lang.ref.WeakReference
 
 /** 居中显示图片 */
 class CenterImageSpan : ImageSpan {
+
+    private var drawableRef: WeakReference<Drawable>? = null
+    private val drawableCache: Drawable
+        get() = drawableRef?.get() ?: drawable.apply {
+            drawableSize(
+                if (width == DRAWABLE_AUTO_SIZE) intrinsicWidth else width,
+                if (height == DRAWABLE_AUTO_SIZE) intrinsicHeight else height
+            )
+            drawableRef = WeakReference(this)
+        }
+
+    var width: Int = DRAWABLE_AUTO_SIZE
+        set(value) {
+            clearDrawableCache()
+            field = value
+        }
+
+    var height: Int = DRAWABLE_AUTO_SIZE
+        set(value) {
+            clearDrawableCache()
+            field = value
+        }
 
     constructor(drawable: Drawable) : super(drawable)
     constructor(drawable: Drawable, source: String) : super(drawable, source)
@@ -26,31 +50,32 @@ class CenterImageSpan : ImageSpan {
     ): Int {
         fm?.let {
             val fontHeight = paint.fontMetricsInt.descent - paint.fontMetricsInt.ascent
-            val imageHeight = drawable.bounds.height()
+            val imageHeight = drawableCache.bounds.height()
             it.ascent = paint.fontMetricsInt.ascent - ((imageHeight - fontHeight) / 2.0f).toInt()
             it.top = it.ascent
             it.descent = it.ascent + imageHeight
             it.bottom = it.descent
         }
-        return drawable.bounds.right
+        return drawableCache.bounds.right
     }
 
     override fun draw(
-        canvas: Canvas,
-        text: CharSequence?,
-        start: Int,
-        end: Int,
-        x: Float,
-        top: Int, // line top,It is the largest top in line.
-        y: Int, // baseline y position
-        bottom: Int, // line bottom,contain lineSpacingExtra if there is more than one line.
-        paint: Paint
+        canvas: Canvas, text: CharSequence?, start: Int, end: Int,
+        x: Float, top: Int, y: Int, bottom: Int, paint: Paint
     ) {
         val fontHeight = paint.fontMetricsInt.descent - paint.fontMetricsInt.ascent
-        val imageAscent = paint.fontMetricsInt.ascent - ((drawable.bounds.height() - fontHeight) / 2.0f).toInt()
+        val imageAscent = paint.fontMetricsInt.ascent - ((drawableCache.bounds.height() - fontHeight) / 2.0f).toInt()
         canvas.save()
         canvas.translate(x, (y + imageAscent).toFloat())
-        drawable.draw(canvas)
+        drawableCache.draw(canvas)
         canvas.restore()
+    }
+
+    fun clearDrawableCache() {
+        drawableRef?.clear()
+    }
+
+    companion object {
+        const val DRAWABLE_AUTO_SIZE = -1
     }
 }
